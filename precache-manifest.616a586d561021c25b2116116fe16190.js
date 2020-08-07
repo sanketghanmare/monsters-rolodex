40107ae12924eeee9dfaa0d84712a444c48d022f
Sanket Ghanmare <sanketghanmare@gmail.com>
2020-08-07 17:30:43 -0400

    Updates

---
+++ precache-manifest.616a586d561021c25b2116116fe16190.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45456736e4c413ff5d207d4dac27f5db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac4784b4504bdd5af82d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "614c29f75c0619efca64",
+    "url": "/static/js/2.051a2960.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.051a2960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac4784b4504bdd5af82d",
+    "url": "/static/js/main.bcd28e0f.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
